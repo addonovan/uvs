533cb2829d9a3f30c80b0cbc378740924c3c36d9
addonovan <austin@addonovan.com>
2019-03-20 15:47:05 -0500

    initial commit

---
+++ lidar/src/main.cpp
@@ -0,0 +1,46 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/LaserScan.h>
+
+double calculate_angle(int distance) {
+    const double THRESHOLD = 200;
+    if (distance < THRESHOLD) {
+        return pow(THRESHOLD - distance, 2) * 0.00003;
+    }
+
+    return 0;
+}
+
+//
+// Boilerplate ROS
+//
+
+using Output = std_msgs::Float64;
+using Input = sensor_msgs::LaserScan;
+
+ros::Publisher* publisher = nullptr;
+
+void on_lidar_message(const Input::ConstPtr& message) {
+    ROS_INFO("Range 0: %d", message->ranges[0]);
+
+    Output msg;
+    msg.data = calculate_angle(message->ranges[0]); 
+    publisher->publish(msg);
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "lidar");
+    ros::NodeHandle n;
+
+    // set up the publisher
+    ros::Publisher pub = n.advertise<Output>("delta_commanded_theta", 1);
+    publisher = &pub;
+
+    // set up the subscriber
+    ros::Subscriber sub = n.subscribe("scan", 1, on_lidar_message);
+
+    // run endlessly
+    ros::spin();
+    return 0;
+}
+
