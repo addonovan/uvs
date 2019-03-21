63e926192e633e36c15e6695857efc3ce77c212d
addonovan <austin@addonovan.com>
2019-03-20 23:08:48 -0500

    warn if calculations take too long

--- lidar/src/main.cpp
+++ lidar/src/main.cpp
@@ -1,13 +1,43 @@
+#include <ctime>
+#include <chrono>
+
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/LaserScan.h>
+
 #include <deflection.hpp>
 #include <math.hpp>
 
 ros::Publisher* publisher = nullptr;
 
 void on_lidar_message(const sensor_msgs::LaserScan::ConstPtr& message) {
+    using namespace std::chrono;
+    using hrclock = high_resolution_clock;
+
+#ifndef NDEBUG
+    hrclock::time_point start = hrclock::now();
+#endif
     Degree deflection = calculate_deflection(message);
+#ifndef NDEBUG
+    hrclock::time_point end = hrclock::now();
+
+    // ambiguous call to operator- here, so we have to manually choose which
+    // template we want to instantiate (this part is 100% my fault but whatever)
+    auto diff = std::chrono::operator-(end, start);
+    auto calc_duration = duration_cast<duration<double>>(diff);
+
+    // if we took more than 1/4th of the time needed to complete a scan, then
+    // we need to issue a warning because that's bad!
+    double calculation_time = calc_duration.count();
+    double max_allowable_time = message->scan_time / 4;
+    if (calculation_time > max_allowable_time) {
+        ROS_WARN(
+                "Calculation time (%1.5lfs) exceeded maximum allowed time (%1.5fs)",
+                calculation_time,
+                max_allowable_time
+        );
+    }
+#endif
 
     // for large angles we should report the changes we're making
     if (deflection >= 1.0 || deflection <= -1.0) {
