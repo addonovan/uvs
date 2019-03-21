df620d1a0b67d2e1d48e3bbfbde11c311fe7bd5d
addonovan <austin@addonovan.com>
2019-03-20 22:05:31 -0500

    cleaned up utility code a bit

--- lidar/include/utility.hpp
+++ lidar/include/utility.hpp
@@ -5,30 +5,31 @@ T operator-(const T& lhs, const T& rhs) {
     return lhs + -rhs;
 }
 
-template<typename T, typename U>
-U operator*(const T& lhs, const U& rhs) {
-    return rhs * lhs;
-}
+#define COMMUTATIVITY(op) \
+    template<typename T, typename U>\
+    U operator op (const T& lhs, const U& rhs) { return rhs * lhs; }
 
-template<typename T, typename U>
-void operator+=(T& lhs, const U& rhs) {
-    lhs = lhs + rhs;
-}
+COMMUTATIVITY(+);
+COMMUTATIVITY(*);
 
-template<typename T, typename U>
-void operator-=(T& lhs, const U& rhs) {
-    lhs = lhs - rhs;
-}
+#undef COMMUTATIVITY
 
-template<typename T, typename U>
-void operator*=(T& lhs, const U& rhs) {
-    lhs = lhs * rhs;
-}
+#define _CREATE_IDENTIFIER(a, b) a ## b
+#define CREATE_IDENTIFIER(a, b) _CREATE_IDENTIFIER(a, b) 
+#define ASSIGNMENT(op) \
+    template<typename T, typename U> \
+    void operator CREATE_IDENTIFIER(op, =) (T& lhs, const U& rhs) { \
+        lhs = lhs op rhs; \
+    }
 
-template<typename T, typename U>
-void operator/=(T& lhs, const U& rhs) {
-    lhs = lhs / rhs;
-}
+ASSIGNMENT(+);
+ASSIGNMENT(-);
+ASSIGNMENT(*);
+ASSIGNMENT(/);
+
+#undef ASSIGNMENT
+#undef CREATE_IDENTIFIER
+#undef _CREATE_IDENTIFIER
 
 template<typename T, typename U>
 bool operator<=(const T& lhs, const U& rhs) {
