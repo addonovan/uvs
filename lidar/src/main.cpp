0d6a828ce376216d6dce87bf6f0f252ecbcf0ab8
addonovan <austin@addonovan.com>
2019-03-23 13:16:29 -0500

    simplify everything a whole bunch

--- lidar/src/main.cpp
+++ lidar/src/main.cpp
@@ -1,54 +1,81 @@
-#include <ctime>
-#include <chrono>
-
+#include <cmath>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/LaserScan.h>
 
-#include <deflection.hpp>
-#include <math.hpp>
+const double PI = 3.1416; // rounded up for comparisons
+const double HALF_PI = PI / 2;
+const double TAU = 2 * PI;
 
-ros::Publisher* publisher = nullptr;
+struct Reading { double angle, range; };
+
+double get_angle(double min, double step, int i) {
+    double angle = PI - (min + (step * i));
+    angle = -(angle - TAU * std::floor((angle + PI)/(2 * PI)));
+    return angle;
+}
+
+double THRESHOLD = 1.00; // m
+bool is_valid_reading(
+        double range_min,
+        double range_max,
+        double range, 
+        double angle
+) {
+    if (std::isnan(range)) return false;
+    if (range < range_min) return false;
+    if (range > range_max) return false;
+    if (!std::isfinite(range)) return false;
+    if (range < THRESHOLD) return false;
+
+    return angle >= -HALF_PI && angle <= HALF_PI;
+}
+
+Reading find_min_reading(const sensor_msgs::LaserScan::ConstPtr& message) {
+    double angle_min = message->angle_min;
+    double angle_step = message->angle_increment;
+
+    double range_min = message->range_min;
+    double range_max = message->range_max;
+
+    Reading min_reading;
+    min_reading.range = 1.0 / 0.0;
+    min_reading.angle = 1.0 / 0.0;
+
+    int i = 0;
+    for (const double range : message->ranges) {
+        double angle = get_angle(angle_min, angle_step, i++); 
+
+        if (!is_valid_reading(range_min, range_max, range, angle)) {
+            continue;
+        }
+
+        if (range < min_reading.range) {
+            min_reading.range = range;
+            min_reading.angle = angle;
+        }
+    }
+
+    return min_reading;
+}
+
+//
+// ROS Stuff
+//
+
+ros::Publisher* angle_pub = nullptr;
+ros::Publisher* range_pub = nullptr;
 
 void on_lidar_message(const sensor_msgs::LaserScan::ConstPtr& message) {
-    using namespace std::chrono;
-    using hrclock = high_resolution_clock;
+    Reading reading = find_min_reading(message);
 
-#ifndef NDEBUG
-    hrclock::time_point start = hrclock::now();
-#endif
+    std_msgs::Float64 angle;
+    angle.data = reading.angle; 
+    angle_pub->publish(angle);
 
-    Degree deflection = calculate_deflection(message);
-
-#ifndef NDEBUG
-    hrclock::time_point end = hrclock::now();
-
-    // ambiguous call to operator- here, so we have to manually choose which
-    // template we want to instantiate (this part is 100% my fault but whatever)
-    auto calc_duration = duration_cast<duration<double>>(end - start);
-
-    // if we took more than 1/4th of the time needed to complete a scan, then
-    // we need to issue a warning because that's bad!
-    double calculation_time = calc_duration.count();
-    double max_allowable_time = message->scan_time / 4;
-    if (calculation_time > max_allowable_time) {
-        ROS_WARN(
-                "Calculation time (%1.5lfs) exceeded maximum allowed time (%1.5fs)",
-                calculation_time,
-                max_allowable_time
-        );
-    }
-#endif
-
-    // for large angles we should report the changes we're making
-    if (deflection >= 1.0 || deflection <= -1.0) {
-        ROS_INFO("Deflecting by %f°", deflection.as_double());
-    }
-
-    // respond with a 64-bit float telling by how many radians we should deflect
-    std_msgs::Float64 msg;
-    msg.data = deflection.as_radian().as_double(); 
-    publisher->publish(msg);
+    std_msgs::Float64 range;
+    range.data = reading.range; 
+    range_pub->publish(range);
 }
 
 int main(int argc, char** argv) {
@@ -56,8 +83,11 @@ int main(int argc, char** argv) {
     ros::NodeHandle n;
 
     // set up the publisher
-    ros::Publisher pub = n.advertise<std_msgs::Float64>("theta_deflection", 1);
-    publisher = &pub;
+    ros::Publisher angle_pub = n.advertise<std_msgs::Float64>("min_lidar_angle", 1);
+    ::angle_pub = &angle_pub;
+
+    ros::Publisher range_pub = n.advertise<std_msgs::Float64>("min_lidar_range", 1);
+    ::range_pub = &range_pub;
 
     // set up the subscriber
     ros::Subscriber sub = n.subscribe("scan", 1, on_lidar_message);
