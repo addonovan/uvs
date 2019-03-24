f21dd6bbee317b9e3c48832b5875d48b52d20a1c
addonovan <austin@addonovan.com>
2019-03-24 17:58:30 -0500

    add more rigorous tests

--- reactive_controls/test/test_units.cpp
+++ reactive_controls/test/test_units.cpp
@@ -1,20 +1,68 @@
 #include <gtest/gtest.h>
 
-#include <units.hpp>
+#include <numeric_types.hpp>
 
-TEST(TestUnitValues, CentimeterOperators) {
-    auto a = Centimeter{10};
-    auto b = Centimeter{5};
-    auto c = Centimeter{17};
+TEST(TestNumericTypes, additiveOperators) {
+    struct TestType : NumericType<TestType, int> {
+        explicit TestType(int inner) : NumericType(inner) {}
+    };
+
+    auto a = TestType{10};
+    auto b = TestType{5};
+    auto c = TestType{17};
 
     ASSERT_EQ(*(a + b), 15);
     ASSERT_EQ(*(a - b), 5);
+    ASSERT_EQ(*(a + c), 27);
+    ASSERT_EQ(*(a - c), -7);
+}
 
-    ASSERT_EQ(*(a * 5), 50);
-    ASSERT_EQ(*(a / 5), 2);
+TEST(TestNumericTypes, scalarOperators) {
+    struct TestType : NumericType<TestType, int> {
+        explicit TestType(int inner) : NumericType(inner) {}
+    };
 
-    a += c; ASSERT_EQ(*a, 27);
-    a -= c; ASSERT_EQ(*a, 10);
-    a *= 6; ASSERT_EQ(*a, 60);
-    a /= 6; ASSERT_EQ(*a, 10);
+    auto a = TestType{25};
+    auto b = TestType{38};
+
+    ASSERT_EQ(*(a * 5), 125);
+    ASSERT_EQ(*(b * 10), 380);
+
+    ASSERT_EQ(*(a / 10), 2);
+    ASSERT_EQ(*(b / 5), 7);
+}
+
+TEST(TestNumericTypes, partialOrdering) {
+    struct TestType : NumericType<TestType, int>, PartialOrdering<TestType> {
+        explicit TestType(int inner) : NumericType(inner) {}
+    };
+
+    auto a = TestType{12};
+    auto b = TestType{78};
+
+    ASSERT_TRUE(b > a);
+    ASSERT_TRUE(a < b);
+
+    ASSERT_FALSE(a > b);
+    ASSERT_FALSE(b < a);
+}
+
+TEST(TestNumericTypes, totalOrdering) {
+    struct TestType : NumericType<TestType, int>, TotalOrdering<TestType> {
+        explicit TestType(int inner) : NumericType(inner) {}
+    };
+
+    auto a = TestType{12};
+    auto b = TestType{78};
+    auto c = TestType{12};
+
+    ASSERT_TRUE(b >= a);
+    ASSERT_TRUE(a <= b);
+
+    ASSERT_FALSE(a >= b);
+    ASSERT_FALSE(b <= a);
+
+    ASSERT_TRUE(a == c);
+    ASSERT_TRUE(a >= c);
+    ASSERT_TRUE(a <= c);
 }
