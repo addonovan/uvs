6c62b655f86e514e4cbe208c98377dea39e74e01
addonovan <austin@addonovan.com>
2019-03-30 14:23:02 -0500

    include required ros messages

--- reactive_controls/src/main.rs
+++ reactive_controls/src/main.rs
@@ -1,3 +1,8 @@
+mod messages;
+pub use self::messages::*;
+
 fn main() {
+    rosrust::init("uvs_reactive");
+
     println!("Hello, world!");
 }
---
+++ reactive_controls/src/messages.rs
@@ -0,0 +1,3 @@
+use rosrust::rosmsg_include;
+
+rosmsg_include!(std_msgs/UInt16, sensor_msgs/LaserScan);
